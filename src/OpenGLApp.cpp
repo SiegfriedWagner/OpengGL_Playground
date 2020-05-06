5a8a4f5974f73c7e08823a45aa97a93b822e7684
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-05-06 15:09:56 +0200

    colorful square

--- src/OpenGLApp.cpp
+++ src/OpenGLApp.cpp
@@ -3,15 +3,19 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 Color;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"   Color = aColor;\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 Color;\n"
 	"void main()\n"
 	"{\n"
-    	"FragColor = vec4(1.0f, 0.5f, 1.0f, 1.0f);\n"
+    	"FragColor = vec4(Color, 1.0f);\n"
 	"}\0";
 
 OpenGLApp::OpenGLApp(int width, int height) 
@@ -44,36 +48,49 @@ bool OpenGLApp::PrepareBuffers() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 	//init buffers
-	float vertices[] = { // aka pozycje
-    	-1.0f, -1.0f, 0.0f,
-     	1.0f, -1.0f, 0.0f,
-    	1.0f,  1.0f, 0.0f,
-		-1.0f, 1.0f, 0.0f
+	float vertices[] = {
+		//postions          // colors
+    	-1.0f, -1.0f, 0.0f,	 1.0f, 0.0f, 0.0f,
+     	 1.0f, -1.0f, 0.0f,  1.0f, 0.0f, 1.0f, 
+    	 1.0f,  1.0f, 0.0f,  0.0f, 1.0f, 0.0f, 
+		-1.0f, 1.0f, 0.0f,   0.0f, 1.0f, 1.0f, 
 	};
+
 	uint indices[] = {
 		0, 1, 3,
 		1, 2, 3
 	};
+/* 	float colorRGBA[] {
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	}; */
 	// creating buffers 
 	glGenVertexArrays(1, &vao);
-	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &vbo);	
 	glGenBuffers(1, &ebo);
 	// binding buffers
 	glBindVertexArray(vao);
+
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
 		sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 
-		3 * sizeof(float), (void*)0);
+		6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 	// some unexplained binding bellow
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	//glBindVertexArray(0);
 	return true;
 }
 void OpenGLApp::loop() {
@@ -84,4 +101,5 @@ void OpenGLApp::loop() {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	// glDrawArrays(GL_TRIANGLES, 0, 3);
 }
