f914d9a6c136195379cdf1d22c02576882faea4c
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-05-10 11:33:20 +0200

    celanup

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,30 +4,6 @@
 #include <fstream>
 #include <sstream>
 
-bool checkShader(GLuint shader) {
-    int  success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "[Error] Shader compilation failed\n"; std::cout << infoLog << std::endl;
-    }
-    return success;
-}
-
-bool checkShaderProgram(GLuint shaderProgram) {
-    int  success;
-    char infoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "[Error] Program linking failed\n"; std::cout << infoLog << std::endl;
-    }
-    return success;
-}
-
 std::string readFile(const std::string &path) {
     std::ifstream file;
     file.open(path);
